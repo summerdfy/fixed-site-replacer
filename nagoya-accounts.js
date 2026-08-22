2389c56e1e7b49d142f7e89763d96f6e9c79becc
summerdfy <18758188023@163.com>
2026-08-22 23:44:07 +0800

    update: 目标链接新页签 + 涩谷/名古屋双账号锁定区

---
+++ nagoya-accounts.js
@@ -0,0 +1,250 @@
+const NAGOYA_ACCOUNTS = [
+  {
+    "email": "DellaLaughman425@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "IrmaRolfsonkktim@hotmail.com",
+    "time": "11:00"
+  },
+  {
+    "email": "GrandersonMcelduff1486@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "MaisieJoey398@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "KandaceSkibicki26@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "PeretRubie38@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "RalonLeitem1966@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "ThompsonLagerstrom83@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "McgeheeSakata957@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "HoytHeaneyube@hotmail.com",
+    "time": "11:00"
+  },
+  {
+    "email": "NginLie903@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "FairhurstMarjenhoff991@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "SengunEdwina55@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "JoanyLuettgenlvxe@hotmail.com",
+    "time": "13:30"
+  },
+  {
+    "email": "RybowiakLuo805@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "BornBradmon78@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "TisonWorster01@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "SavinonBobson9429@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "FletaDenesiktqeg@hotmail.com",
+    "time": "13:30"
+  },
+  {
+    "email": "SabloneTravino301@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "GondekSettles75@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "BoshersJacquez98@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "TeresaBraunccix@hotmail.com",
+    "time": "13:30"
+  },
+  {
+    "email": "CacatianJenn34@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "PizzaDroney630@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "ChonAttard59@outlook.com",
+    "time": "13:30"
+  },
+  {
+    "email": "UhrhammerSirena7050@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "MagriMillings36@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "IanthaMeihofer9762@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "RemediesCopstead954@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "ShawnaCroninri@hotmail.com",
+    "time": "16:00"
+  },
+  {
+    "email": "BrenchleyClarissa721@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "ImbachForstner943@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "StrassbergAslinger7553@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "PleimanLandini570@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "RancesEllingham5138@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "AntoniaEffertzuiskq@hotmail.com",
+    "time": "16:00"
+  },
+  {
+    "email": "SealWackenhut05@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "BitnerVanepps5351@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "EitnierMoonsommy137@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "OhmieMarbury599@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "SilasWatersjpsdg@hotmail.com",
+    "time": "16:00"
+  },
+  {
+    "email": "MallochDarren516@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "PulseMontrose36@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "BriceAndersonkkis@hotmail.com",
+    "time": "18:30"
+  },
+  {
+    "email": "JacquesYundteucwq@hotmail.com",
+    "time": "18:30"
+  },
+  {
+    "email": "MarionKilbackjykt@hotmail.com",
+    "time": "18:30"
+  },
+  {
+    "email": "VioletBartonxbflm@hotmail.com",
+    "time": "18:30"
+  },
+  {
+    "email": "MiloNicolaslqnj@hotmail.com",
+    "time": "18:30"
+  },
+  {
+    "email": "StgeorgeHeupel873@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "PopularisNona4147@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "TaniaDouglasjff@hotmail.com",
+    "time": "18:30"
+  },
+  {
+    "email": "WyerFarrare8415@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "BartekBothwell3774@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "DargieHeiken8251@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "DessieSchummic@hotmail.com",
+    "time": "18:30"
+  },
+  {
+    "email": "EldaEmmerichvh@hotmail.com",
+    "time": "18:30"
+  },
+  {
+    "email": "RenfredMichonski22@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "GlyndaHurrle64@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "JaynaGracy9185@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "SojoKleiboeker2297@outlook.com",
+    "time": "18:30"
+  },
+  {
+    "email": "CarmelaPredovicrzh@hotmail.com",
+    "time": "18:30"
+  }
+];
---
+++ shibuya-accounts.js
@@ -0,0 +1,306 @@
+const SHIBUYA_ACCOUNTS = [
+  {
+    "email": "ZychKaczor7721@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "ZalazarMounce77@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "MalamudVis06@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "SterkSchmiege44@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "PenatacCollege712@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "MihmArceneaux13@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "SmillieFrasier5036@outlook.com",
+    "time": "11:00"
+  },
+  {
+    "email": "TrosienTeichert2171@outlook.com",
+    "time": "12:00"
+  },
+  {
+    "email": "SprewellOshiro06@outlook.com",
+    "time": "12:00"
+  },
+  {
+    "email": "LeekerGieck715@outlook.com",
+    "time": "12:00"
+  },
+  {
+    "email": "GaspardSherburn778@outlook.com",
+    "time": "12:00"
+  },
+  {
+    "email": "BallouErner1635@outlook.com",
+    "time": "12:00"
+  },
+  {
+    "email": "RochlinBudnick41@outlook.com",
+    "time": "12:00"
+  },
+  {
+    "email": "HartiganJump076@outlook.com",
+    "time": "12:00"
+  },
+  {
+    "email": "RosensteelBodenschatz99@outlook.com",
+    "time": "12:00"
+  },
+  {
+    "email": "LisenbyWhitacker62@outlook.com",
+    "time": "13:00"
+  },
+  {
+    "email": "SmyerRheault92@outlook.com",
+    "time": "13:00"
+  },
+  {
+    "email": "RuehlenDezarn1843@outlook.com",
+    "time": "13:00"
+  },
+  {
+    "email": "HawkinsEireann13@outlook.com",
+    "time": "13:00"
+  },
+  {
+    "email": "DalesandroScallorn689@outlook.com",
+    "time": "13:00"
+  },
+  {
+    "email": "SerrettDini068@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "LatikerBurgraff49@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "IfflandKrefft705@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "ShurrKetcherside17@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "CasaliTegtmeyer6659@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "GablesAurelia465@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "DoSames33@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "MakinoMcgillen99@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "KutscherBianchini443@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "GoebelGuenevere678@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "MccroreyLeflore0522@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "GlaweQuartieri1248@outlook.com",
+    "time": "14:00"
+  },
+  {
+    "email": "ShillerBierner746@outlook.com",
+    "time": "15:00"
+  },
+  {
+    "email": "HanzlLjungquist7730@outlook.com",
+    "time": "15:00"
+  },
+  {
+    "email": "EidschunChiquita0237@outlook.com",
+    "time": "15:00"
+  },
+  {
+    "email": "MatrejekRodde981@outlook.com",
+    "time": "15:00"
+  },
+  {
+    "email": "TanimotoJeremiah917@outlook.com",
+    "time": "15:00"
+  },
+  {
+    "email": "KucharMctiernan421@outlook.com",
+    "time": "15:00"
+  },
+  {
+    "email": "GuenevereLyke71@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "DerivanEda813@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "ArmandManusyants19@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "EiselmanPhilip493@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "WaxmanKrzak019@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "SauberanHautala822@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "HryckoHolk1421@outlook.com",
+    "time": "16:00"
+  },
+  {
+    "email": "SabinoDryden55@outlook.com",
+    "time": "17:00"
+  },
+  {
+    "email": "UmbrellRubarts267@outlook.com",
+    "time": "17:00"
+  },
+  {
+    "email": "BowersoxDonald55@outlook.com",
+    "time": "17:00"
+  },
+  {
+    "email": "FelmetHiatt89@outlook.com",
+    "time": "17:00"
+  },
+  {
+    "email": "HeviaBellmay023@outlook.com",
+    "time": "17:00"
+  },
+  {
+    "email": "RynkowskiSchaper555@outlook.com",
+    "time": "17:00"
+  },
+  {
+    "email": "MizwickiHurla306@outlook.com",
+    "time": "17:00"
+  },
+  {
+    "email": "NarasimhanRosanne42@outlook.com",
+    "time": "17:00"
+  },
+  {
+    "email": "MitsudaTreinen331@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "GeppertCarbon6308@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "CianciHalder467@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "JewsShollenberger586@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "HappennySliman9059@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "CarolinEmbleton302@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "SchwambergerBuell390@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "SalinaManzella072@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "LoreneGirdley630@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "FissoriZurkuhlen9656@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "VassenDurney6729@outlook.com",
+    "time": "18:00"
+  },
+  {
+    "email": "UsticoTsutsumi7546@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "AbdallahUnderkoffler6749@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "RoehlPinzon179@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "SmitheyHatada09@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "BevilleGilliard7910@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "SadarLaubscher127@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "ZaffalonPoli76@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "MarkumHalbershtam67@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "MonforteEvia77@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "BosketWatkins29@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "PuigGarski263@outlook.com",
+    "time": "19:00"
+  },
+  {
+    "email": "ShuffstallShanon7302@outlook.com",
+    "time": "19:00"
+  }
+];
