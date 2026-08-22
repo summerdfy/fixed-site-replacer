eda7d09aa96952fc3ed913f91564efcf6db65cf4
summerdfy <18758188023@163.com>
2026-08-23 01:58:17 +0800

    fix: 3个BUG - 小屏上下排列/东京密码Qwe123123/强制下载

--- shibuya-accounts.js
+++ shibuya-accounts.js
@@ -1,382 +1,382 @@
 const SHIBUYA_ACCOUNTS = [
   {
     "email": "ZychKaczor7721@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "11:00"
   },
   {
     "email": "ZalazarMounce77@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "11:00"
   },
   {
     "email": "MalamudVis06@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "11:00"
   },
   {
     "email": "SterkSchmiege44@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "11:00"
   },
   {
     "email": "PenatacCollege712@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "11:00"
   },
   {
     "email": "MihmArceneaux13@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "11:00"
   },
   {
     "email": "SmillieFrasier5036@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "11:00"
   },
   {
     "email": "TrosienTeichert2171@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "12:00"
   },
   {
     "email": "SprewellOshiro06@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "12:00"
   },
   {
     "email": "LeekerGieck715@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "12:00"
   },
   {
     "email": "GaspardSherburn778@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "12:00"
   },
   {
     "email": "BallouErner1635@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "12:00"
   },
   {
     "email": "RochlinBudnick41@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "12:00"
   },
   {
     "email": "HartiganJump076@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "12:00"
   },
   {
     "email": "RosensteelBodenschatz99@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "12:00"
   },
   {
     "email": "LisenbyWhitacker62@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "13:00"
   },
   {
     "email": "SmyerRheault92@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "13:00"
   },
   {
     "email": "RuehlenDezarn1843@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "13:00"
   },
   {
     "email": "HawkinsEireann13@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "13:00"
   },
   {
     "email": "DalesandroScallorn689@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "13:00"
   },
   {
     "email": "SerrettDini068@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "LatikerBurgraff49@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "IfflandKrefft705@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "ShurrKetcherside17@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "CasaliTegtmeyer6659@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "GablesAurelia465@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "DoSames33@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "MakinoMcgillen99@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "KutscherBianchini443@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "GoebelGuenevere678@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "MccroreyLeflore0522@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "GlaweQuartieri1248@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "14:00"
   },
   {
     "email": "ShillerBierner746@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "15:00"
   },
   {
     "email": "HanzlLjungquist7730@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "15:00"
   },
   {
     "email": "EidschunChiquita0237@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "15:00"
   },
   {
     "email": "MatrejekRodde981@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "15:00"
   },
   {
     "email": "TanimotoJeremiah917@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "15:00"
   },
   {
     "email": "KucharMctiernan421@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "15:00"
   },
   {
     "email": "GuenevereLyke71@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "16:00"
   },
   {
     "email": "DerivanEda813@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "16:00"
   },
   {
     "email": "ArmandManusyants19@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "16:00"
   },
   {
     "email": "EiselmanPhilip493@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "16:00"
   },
   {
     "email": "WaxmanKrzak019@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "16:00"
   },
   {
     "email": "SauberanHautala822@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "16:00"
   },
   {
     "email": "HryckoHolk1421@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "16:00"
   },
   {
     "email": "SabinoDryden55@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "17:00"
   },
   {
     "email": "UmbrellRubarts267@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "17:00"
   },
   {
     "email": "BowersoxDonald55@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "17:00"
   },
   {
     "email": "FelmetHiatt89@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "17:00"
   },
   {
     "email": "HeviaBellmay023@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "17:00"
   },
   {
     "email": "RynkowskiSchaper555@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "17:00"
   },
   {
     "email": "MizwickiHurla306@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "17:00"
   },
   {
     "email": "NarasimhanRosanne42@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "17:00"
   },
   {
     "email": "MitsudaTreinen331@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "GeppertCarbon6308@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "CianciHalder467@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "JewsShollenberger586@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "HappennySliman9059@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "CarolinEmbleton302@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "SchwambergerBuell390@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "SalinaManzella072@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "LoreneGirdley630@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "FissoriZurkuhlen9656@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "VassenDurney6729@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "18:00"
   },
   {
     "email": "UsticoTsutsumi7546@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "AbdallahUnderkoffler6749@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "RoehlPinzon179@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "SmitheyHatada09@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "BevilleGilliard7910@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "SadarLaubscher127@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "ZaffalonPoli76@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "MarkumHalbershtam67@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "MonforteEvia77@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "BosketWatkins29@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "PuigGarski263@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   },
   {
     "email": "ShuffstallShanon7302@outlook.com",
-    "pass": "Qwe23123",
+    "pass": "Qwe123123",
     "time": "19:00"
   }
 ];
